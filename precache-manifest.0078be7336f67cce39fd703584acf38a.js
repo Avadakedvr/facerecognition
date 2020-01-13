473ba0f1b8884c89ca49b21e46a89baeaacc6765
Denis <denis.bivol@live.com>
2020-01-13 12:22:40 +0000

    Updates

---
+++ precache-manifest.0078be7336f67cce39fd703584acf38a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c47718585ff6a837f23d5929aaa61951",
+    "url": "/facerecognition/index.html"
+  },
+  {
+    "revision": "185e86a9385a41173575",
+    "url": "/facerecognition/static/css/2.19dd4d8c.chunk.css"
+  },
+  {
+    "revision": "a617f1132486b8700c1e",
+    "url": "/facerecognition/static/css/main.249af21d.chunk.css"
+  },
+  {
+    "revision": "185e86a9385a41173575",
+    "url": "/facerecognition/static/js/2.de5e9b23.chunk.js"
+  },
+  {
+    "revision": "0330a48739a1a164177abbdbf45e393f",
+    "url": "/facerecognition/static/js/2.de5e9b23.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a617f1132486b8700c1e",
+    "url": "/facerecognition/static/js/main.978f4cc0.chunk.js"
+  },
+  {
+    "revision": "bb08d7401cf0d0898d7c",
+    "url": "/facerecognition/static/js/runtime-main.644a34d0.js"
+  },
+  {
+    "revision": "79a81538b8de7237410803cd0c4fb9e7",
+    "url": "/facerecognition/static/media/circuit.79a81538.svg"
+  },
+  {
+    "revision": "3f1fbe79ec6df4a3c677ce0d758cdce8",
+    "url": "/facerecognition/static/media/cour.3f1fbe79.ttf"
+  }
+]);
